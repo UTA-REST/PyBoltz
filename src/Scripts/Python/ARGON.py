1c0e9ba5b93579d0581e13fed89c6af7d0b9c52f
basharatoum <atoumbashar@gmail.com>
2018-11-26 22:57:24 -0600

    More Fixes

--- src/Scripts/Python/ARGON.py
+++ src/Scripts/Python/ARGON.py
@@ -89,7 +89,7 @@ def Gas2(object):
 
     IOFFION = [0 for x in range(10)]
     IOFFN = [0 for x in range(44)]
-    for j in range(0, NION):
+    for j in range(0,object.NION):
         for i in range(0, 4000):
             if object.EG[i] > object.EION[j]:
                 IOFFION[j] = i - 1

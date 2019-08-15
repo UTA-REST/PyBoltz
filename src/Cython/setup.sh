13b49fee017e8d5beb06fa710c61b43e9762a170
basharatoum <atoumbashar@gmail.com>
2019-08-15 04:02:09 -0500

    added gases.npy to Examples folder

--- src/Cython/setup.sh
+++ src/Cython/setup.sh
@@ -1,8 +1,10 @@
-
-# setup the enviorment                                                                             
+# setup the enviorment
 export PYTHONPATH=$PYTHONPATH:$PWD
+
+# setup the cross sections database (gases.npy)
 python3 Setup_npy.py
 
+# build the code
 python3 setup.py clean
 
 python3 setup.py build_ext --inplace

4c9b8c318dc010aade6cac0a9f4f5a1d2865c22f
Ben Jones <ben.jones@uta.edu>
2019-08-31 22:22:17 -0500

    Move gases to folder

--- src/Cython/setup.py
+++ src/Cython/setup.py
@@ -1,4 +1,7 @@
 from distutils.core import setup
 from Cython.Build import cythonize
 import  numpy
-setup(ext_modules=cythonize(["*.pyx"]), inlude_dirs=[numpy.get_include(),'.'])
+print(numpy.get_include())
+
+setup(ext_modules=cythonize(["gases/*.pyx"]), include_dirs=[numpy.get_include(),'.'])
+setup(ext_modules=cythonize(["*.pyx"]), include_dirs=[numpy.get_include(),'.'])

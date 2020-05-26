7f2272a83420d9c9a563beca197b4f4b0f51dd04
basharatoum <atoumbashar@gmail.com>
2020-05-26 13:22:25 -0500

    changes

--- setup.py
+++ setup.py
@@ -45,7 +45,7 @@ setup(
     python_requires='>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*, <4',
     package_dir={'PyBoltz': 'PyBoltz'},
 
-    install_requires=['numpy','cython','git+https://github.com/UTA-REST/PyGasMix#egg=PyGasMix'],  # Optional
+    install_requires=['numpy','cython','-e git+https://github.com/UTA-REST/PyGasMix.git#egg=PyGasMix'],  # Optional
     #include_package_data = True,
     #'''package_data={  # Optional
     #    'PyGasMix': ['./PyGasMix/*.pxd','./PyGasMix/*.pxd'],

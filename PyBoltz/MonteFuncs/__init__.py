f1e387e80739edc302f6b39e67b723c1bd4dba1c
basharatoum <atoumbashar@gmail.com>
2020-05-27 17:49:21 -0500

    changes

--- PyBoltz/MonteFuncs/__init__.py
+++ PyBoltz/MonteFuncs/__init__.py
@@ -1,7 +1,7 @@
-from MonteFuncs import MONTE
-from MonteFuncs import MONTET
-from MonteFuncs import MONTEB
-from MonteFuncs import MONTEBT
-from MonteFuncs import MONTEC
-from MonteFuncs import MONTECT
+import MONTE
+import MONTET
+import MONTEB
+import MONTEBT
+import MONTEC
+import MONTECT
 __all__=['MONTE','MONTEB','MONTEBT','MONTEC','MONTECT','MONTET']

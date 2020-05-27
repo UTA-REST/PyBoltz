5df62f4e604953d5b9d535a94d774d41f726624e
basharatoum <atoumbashar@gmail.com>
2020-05-27 15:41:44 -0500

    push

--- PyBoltz/Boltz.pyx
+++ PyBoltz/Boltz.pyx
@@ -5,8 +5,8 @@ from libc.string cimport memset
 import Setups
 import Mixers
 import EnergyLimits
-from PyBoltz.MonteFuncs cimport *
-from PyBoltz.Townsend cimport *
+from PyBoltz.MonteFuncs import *
+from PyBoltz.Townsend import *
 from PyGasMix.Gasmix cimport Gasmix
 
 
